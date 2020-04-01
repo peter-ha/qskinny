7abc0125eef4a853840673ff33fd6d9a8b1b59c3
Peter Hartmann <peter@edelhirsch.io>
2020-04-01 09:48:10 +0200

    Extend stack box example

--- examples/stack-layout/main.cpp
+++ examples/stack-layout/main.cpp
@@ -8,15 +8,29 @@
 
 #include <QskBox.h>
 #include <QskBoxBorderColors.h>
+#include <QskStackBox.h>
 #include <QskTextLabel.h>
 #include <QskWindow.h>
 
 #include <QGuiApplication>
 
+#define FAKEBOX 0
+
 class FakeStackBox : public QskBox {
 public:
     FakeStackBox( QQuickItem* parent = nullptr ) : QskBox( parent )
     {
+    }
+
+    void addItem( QQuickItem* item )
+    {
+        item->setParent( this );
+        item->setParentItem( this );
+    }
+
+    void setCurrentIndex( int /*index*/ )
+    {
+        // do nothing
     }
 
 protected:
@@ -43,30 +57,39 @@ int main( int argc, char* argv[] )
     SkinnyFont::init( &app );
     SkinnyShortcut::enable( SkinnyShortcut::AllShortcuts );
 
-    auto* box = new FakeStackBox();
-    box->setMargins( 0 );
-    box->setAutoLayoutChildren( true );
-    box->setFixedSize( { 100, 80 } ); // ### hack
-    box->setGradientHint( QskBox::Panel | QskAspect::Color, { Qt::transparent } );
-    box->setBoxBorderColorsHint( QskBox::Panel, { Qt::transparent } );
+#if FAKEBOX
+    auto* stackBox = new FakeStackBox();
+    stackBox->setMargins( 0 );
+    stackBox->setAutoLayoutChildren( true );
+    stackBox->setFixedSize( { 100, 80 } ); // ### hack
+    stackBox->setGradientHint( QskBox::Panel | QskAspect::Color, { Qt::transparent } );
+    stackBox->setBoxBorderColorsHint( QskBox::Panel, { Qt::transparent } );
+#else
+    auto* stackBox = new QskStackBox;
+#endif
 
-    auto* label1 = new QskTextLabel( "control 1", box );
+    auto* label1 = new QskTextLabel( "control 1" );
     label1->setMargins( 10 );
     label1->setObjectName( "1" );
     label1->setBackgroundColor( Qt::blue );
+    stackBox->addItem( label1 );
 
-    auto* label2 = new QskTextLabel( "control 2", box );
+    auto* label2 = new QskTextLabel( "control 2" );
     label2->setMargins( 10 );
     label2->setObjectName( "2" );
     label2->setBackgroundColor( Qt::cyan );
+    stackBox->addItem( label2 );
 
-    auto* label3 = new QskTextLabel( "control 3", box );
+    auto* label3 = new QskTextLabel( "control 3" );
     label3->setMargins( 10 );
     label3->setObjectName( "3" );
     label3->setBackgroundColor( Qt::magenta );
+    stackBox->addItem( label3 );
+
+    stackBox->setCurrentIndex( 2 );
 
     QskWindow window;
-    window.addItem( box );
+    window.addItem( stackBox );
     window.show();
 
     return app.exec();
